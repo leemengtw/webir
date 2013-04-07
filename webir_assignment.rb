368f38d5768a63c4aaada0f144aa89ad883f081d
abc123634 <b98705001@ntu.edu.tw>
2013-04-07 19:04:50 +0800

    Create webir_assignment.rb

---
+++ webir_assignment.rb
@@ -0,0 +1,34 @@
+require 'fileUtils'
+
+#class DocVector
+
+class Term
+  attr_accessor :id, :name, :df, :docList
+	def initialize(id=0, df=0)
+		@id = id
+		@df = df
+		@name = ""
+		@docList = {}
+	end
+	def inspect 
+		"a term of #{name}"
+	end
+end
+
+		
+
+$termHash = {} #存放每個Term物件的陣列
+inverted_index = File.foreach("inverted-index") do |line| #對檔案inverted-index逐行操作
+	line_s = line.split(" ")
+
+	if line_s.size.eql?(3) && line_s[1].eql?("-1") #新unigram
+		term = Term.new(line_s[0], line_s[2]) #id & df
+		puts "term #{term.name}\'s id: #{term.id} df : #{term.df}"
+		$termHash[line_s.size[0]] = term 
+		puts $termHash[line_s.size[0]]
+	elsif line_s.size.eql?(2) #記錄某term在某文件的tf
+		
+	end
+
+end
+
