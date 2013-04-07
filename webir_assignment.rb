5428c81fb89b2464a62c3e24a5927b2e00230f50
abc123634 <b98705001@ntu.edu.tw>
2013-04-07 19:24:10 +0800

    Update webir_assignment.rb

--- webir_assignment.rb
+++ webir_assignment.rb
@@ -3,7 +3,7 @@ require 'fileUtils'
 #class DocVector
 
 class Term
-  attr_accessor :id, :name, :df, :docList
+	attr_accessor :id, :name, :df, :docList
 	def initialize(id=0, df=0)
 		@id = id
 		@df = df
@@ -23,9 +23,9 @@ inverted_index = File.foreach("inverted-index") do |line| #對檔案inverted-ind
 
 	if line_s.size.eql?(3) && line_s[1].eql?("-1") #新unigram
 		term = Term.new(line_s[0], line_s[2]) #id & df
-		puts "term #{term.name}\'s id: #{term.id} df : #{term.df}"
-		$termHash[line_s.size[0]] = term 
-		puts $termHash[line_s.size[0]]
+		#puts "term #{term.name}\'s id: #{term.id} df : #{term.df}"
+		$termHash[line_s.size[0]] = term #存入term的hash
+		puts $termHash[line_s.size[0]].id
 	elsif line_s.size.eql?(2) #記錄某term在某文件的tf
 		
 	end
