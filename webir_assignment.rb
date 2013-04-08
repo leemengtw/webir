4940681f37f41cbbd19ab6d1839cd67fc21c926d
abc123634 <b98705001@ntu.edu.tw>
2013-04-08 15:25:05 +0800

    Fixed Some bug

--- webir_assignment.rb
+++ webir_assignment.rb
@@ -1,3 +1,4 @@
+#讀入inverted-index, vocau檔案，建立每篇文章的維度(term vector)
 require 'fileUtils'
 
 #class DocVector
@@ -8,27 +9,30 @@ class Term
 		@id = id
 		@df = df
 		@name = ""
-		@docList = {}
+		@docList = {} #紀錄term在哪篇文章出現幾次，key為doc id, value為tf
 	end
 	def inspect 
 		"a term of #{name}"
 	end
 end
 
-		
 
-$termHash = {} #存放每個Term物件的陣列
-inverted_index = File.foreach("inverted-index") do |line| #對檔案inverted-index逐行操作
+
+termHash = {} #存放每個Term物件的陣列
+
+current_term = nil
+
+inverted_index = File.foreach("test.txt") do |line| #對檔案inverted-index逐行操作
 	line_s = line.split(" ")
 
-	if line_s.size.eql?(3) && line_s[1].eql?("-1") #新unigram
-		term = Term.new(line_s[0], line_s[2]) #id & df
-		#puts "term #{term.name}\'s id: #{term.id} df : #{term.df}"
-		$termHash[line_s.size[0]] = term #存入term的hash
-		puts $termHash[line_s.size[0]].id
+	if line_s.size.eql?(3) && line_s[1].eql?("-1") #將新unigram加到termHash裡頭並記錄該term的資訊 
+		term = Term.new(line_s[0].to_i, line_s[2].to_i)
+		termHash[term.id] = term  
+		puts "term #{term.name}\'s id: #{term.id} df : #{term.df}"
+		current_term = term.id
 	elsif line_s.size.eql?(2) #記錄某term在某文件的tf
-		
+		termHash[current_term].docList[line_s[0].to_i] = line_s[1].to_i		
 	end
-
 end
 
+termHash.each {|key, value| puts "term #{value.id}: #{value.df}  #{value.docList}"}
